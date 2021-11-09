808c502e88079598589962a48afc34d0013f871f
netooo <kazushi.mitsue@gmail.com>
2021-11-09 09:34:33 +0900

    Add: installer

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 DOT_DIR="$HOME/dotfiles"
 
-echo "start clone..."
+echo "start git clone..."
 if [ ! -d ${DOT_DIR} ]; then
   git clone https://github.com/netooo/dotfiles.git ${DOT_DIR}  
 else
   echo "dotfiles already exists"
 fi
 
-echo "start setup..."
+echo "start symbolic link..."
 cd $HOME
-for file in ".zsh/" ".zshenv" ".zshrc" ".zshrc.alias" ".zshrc.cd" ".zshrc.tracking" do
+for file in ".zsh/" ".zshenv" ".zshrc" ".zshrc.alias" ".zshrc.cd" ".zshrc.tracking"
+do
   [ ! -e $file ] && ln -s ${DOT_DIR}/$file .
 done
