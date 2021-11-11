e40d10ce46fe3df893acee9ef86d12582e859155
netooo <kazushi.mitsue@gmail.com>
2021-11-11 09:41:53 +0900

    Add: setup shells

---
+++ setup_shell.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ZSHPATH=/usr/local/bin/zsh
+
+has() {
+    type "$1" > /dev/null 2>&1
+}
+
+# zsh
+if has "zsh"; then
+  echo 'zsh already installed'
+else
+  echo 'install zsh shell'
+  brew install zsh
+fi
+
+if [ $(echo "$SHELL") == $ZSHPATH ]; then
+  echo 'default shell already zsh'
+else
+  echo 'set default shell to zsh'
+  where zsh
+  sudo sh -c "echo '$ZSHPATH' >> /etc/shells"
+  chsh -s $ZSHPATH
+  echo 'open the new tab'
+fi
+
+# fzf
+if has "fzf"; then
+  echo 'fzf already installed'
+else
+  echo 'install fzf shell'
+  brew install fzf
+fi
